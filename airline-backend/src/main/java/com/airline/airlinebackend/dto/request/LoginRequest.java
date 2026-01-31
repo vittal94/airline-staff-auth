a623e3e6e9bb55a668e036a0e703af9757ed0cd6
evg99 <evg994z@mail.ru>
2026-01-31 20:26:06 +0300

    add dto/request/LoginRequest

---
+++ airline-backend/src/main/java/com/airline/airlinebackend/dto/request/LoginRequest.java
@@ -0,0 +1,29 @@
+package com.airline.airlinebackend.dto.request;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public LoginRequest() {}
+
+    public LoginRequest(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
