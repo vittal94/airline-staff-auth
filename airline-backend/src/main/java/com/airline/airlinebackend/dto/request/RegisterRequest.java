9fe04bdd3cf68d99cb0eb21589ee14c6e301cbca
evg99 <evg994z@mail.ru>
2026-01-31 20:30:06 +0300

    add dto/request/RegisterRequest

---
+++ airline-backend/src/main/java/com/airline/airlinebackend/dto/request/RegisterRequest.java
@@ -0,0 +1,42 @@
+package com.airline.airlinebackend.dto.request;
+
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String role;
+
+    public RegisterRequest() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
