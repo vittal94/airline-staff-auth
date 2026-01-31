a11770bb7c3d86e967a5e45a02bc2c47346fb296
evg99 <evg994z@mail.ru>
2026-01-31 20:33:33 +0300

    add dto/request/ChangePasswordRequest

---
+++ airline-backend/src/main/java/com/airline/airlinebackend/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,33 @@
+package com.airline.airlinebackend.dto.request;
+
+public class ChangePasswordRequest {
+    private String newPassword;
+    private String oldPassword;
+    private String confirmNewPassword;
+
+    public ChangePasswordRequest() {}
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getConfirmNewPassword() {
+        return confirmNewPassword;
+    }
+
+    public void setConfirmNewPassword(String confirmNewPassword) {
+        this.confirmNewPassword = confirmNewPassword;
+    }
+}
