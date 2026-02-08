7fedd96638b5f37416eb6077465305bc86775501
evg99 <evg994z@mail.ru>
2026-02-08 15:37:35 +0300

    added exception/AccountNotApprovedException and test

--- airline-backend/src/main/java/com/airline/airlinebackend/exception/AccountNotApprovedException.java
+++ airline-backend/src/main/java/com/airline/airlinebackend/exception/AccountNotApprovedException.java
@@ -1,4 +1,10 @@
 package com.airline.airlinebackend.exception;
 
-public class AccountNotApprovedException {
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AccountNotApprovedException extends BaseException{
+    public AccountNotApprovedException() {
+        super("Account pending approval. Please wait for administrator approval.",
+                "ACCOUNT_NOT_APPROVED", HttpServletResponse.SC_FORBIDDEN);
+    }
 }
